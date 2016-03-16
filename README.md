069b791e4871c4b660d72538cead9b47aaa376c0
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2016-03-16 22:22:18 +0100

    Upload Version 2.0 - Now with service name for each port number

--- README.md
+++ README.md
@@ -1,44 +1,67 @@
 # PowerShell Async PortScanner
 
-Powerful asynchronus Port-Scanner which returns a custom PowerShell-Object with basic informations about the scanned Port-Range include Port and Status.
+Powerful asynchronus Port-Scanner which returns a custom PowerShell-Object with basic informations about the scanned Port-Range include port number, protocol, service name, service description and status.
 
 ## Description
 
-This is a powerful asynchronus Port-Scanner working with the PowerShell RunspacePool. You can scan any Port-Range you want. 
+This is a powerful asynchronus Port-Scanner working with the PowerShell RunspacePool. You can scan any Port-Range you want. The Result will show you all open ports port number, protocol, service name, service description and status.
     
 This script also work fine along with my asychronus IP-Scanner published on GitHub too. You can easily pipe the output of the IP-Scanner result in this script.
 
 ## Syntax
 
 ```powershell
-.\ScanPortsAsync.ps1 [-IPv4Address] <IPAddress> [[-StartPort] <Int32>] [[-EndPort] <Int32>] [[-Threads] <Int32>] [[-IncludeClosed]] [<CommonParameters>]
+.\ScanPortsAsync.ps1 [-IPv4Address] <IPAddress> [[-StartPort] <Int32>] [[-EndPort] <Int32>] [[-Threads] <Int32>] [[-UpdateListFromIANA]] [<CommonParameters>]
 ```
 
 ## Example
 
-Simple Port Scan
+Scan a specific Port-Range (1-500)
+
 ```powershell
-.\ScanPortsAsync.ps1 -IPv4Address 192.168.1.100 -StartPort 1 -EndPort 5000
+.\ScanPortsAsync.ps1 -IPv4Address 192.168.1.100 -StartPort 1 -EndPort 500 | Format-Table
 ``` 
 
-Show closed Ports in result
-```powershell
-.\ScanPortsAsync.ps1 -IPv4Address 172.16.2.5 -Threads 200 -IncludeClosed
-``` 
+You may want to update the official Service Name and Transport Protocol Port Number Registry from IANA... Just add the parameter "-UpdateListFromIANA".
 
+```powershell
+.\ScanPortsAsync.ps1 -IPv4Address 172.16.2.5 -UpdateListFromIANA
+``` 
+If your PC has enough power, you can use more threads at the same time
+
+```powershell
+.\ScanPortsAsync.ps1 -IPv4Address 172.16.2.5 -Threads 250
+``` 
 
 ## Output 
 
 ```powershell
-Port Status
----- ------
-  21 Open  
-  80 Open  
- 139 Open  
- 443 Open
- 445 Open  
+Port Protocol Service Name Service Description              Status
+---- -------- ------------ -------------------              ------
+  21 tcp      ftp          File Transfer Protocol [Control] open
+  53 tcp      domain       Domain Name Server               open
+  80 tcp      http         World Wide Web HTTP              open
+  80 tcp      www          World Wide Web HTTP              open
+  80 tcp      www-http     World Wide Web HTTP              open
+ 139 tcp      netbios-ssn  NETBIOS Session Service          open
+ 445 tcp      microsoft-ds Microsoft-DS                     open
+``` 
+
+and if no port list is available (should never happend, because it's uploaded on Github)
+
+```powershell
+Port Protocol Status
+---- -------- ------
+  21 tcp      open
+  53 tcp      open
+  80 tcp      open
+ 139 tcp      open
+ 445 tcp      open
 ```
-  
+
+## Offical Port List
+
+* [Service Name and Transport Protocol Port Number Registry - IANA.org](https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xml)
+
 ## ToDo
-- Integrate Port-List
- like: 80 (http), 443 (https) ...
+[x] Integrate Port-List like: 80 (http), 443 (https), etc.
--- Scripts/ScanPortsAsync.ps1
+++ Scripts/ScanPortsAsync.ps1
@@ -9,11 +9,12 @@
 <#
     .SYNOPSIS
     Powerful asynchronus Port-Scanner which returns a custom PowerShell-Object with basic informations about the 
-    scanned Port-Range include Port and Status.
+    scanned Port-Range include port number, protocol, service name, service description and status.
     
     .DESCRIPTION
     This is a powerful asynchronus Port-Scanner working with the PowerShell RunspacePool. You can scan any 
-    Port-Range you want. 
+    Port-Range you want. The Result will show you all open ports port number, protocol, service name,
+    service description and status.
     
     This script also work fine along with my asychronus IP-Scanner published on GitHub too. You can easily
     pipe the output of the IP-Scanner result in this script.
@@ -24,7 +25,10 @@
     .\ScanPortsAsync.ps1 -IPv4Address 172.16.0.1 -StartPort 1 -EndPort 1000
     
     .EXAMPLE
-    .\ScanPortsAsync.ps1 -IPv4Address 192.168.1.100 -IncludeClosed
+    .\ScanPortsAsync.ps1 -IPv4Address 192.168.1.100 -UpdateListFromIANA
+
+    .EXAMPLE
+    .\ScanPortsAsync.ps1 -IPv4Address 192.168.1.100 -Threads 250
     
     .LINK
     Github Profil:         https://github.com/BornToBeRoot
@@ -41,36 +45,39 @@ Param(
 
     [Parameter(
         Position=1,
-        Mandatory=$false,
         HelpMessage='Enter the Start-Port (Default=1)')]
     [Int32]$StartPort=1,
 
     [Parameter(
         Position=2,
-        Mandatory=$false,
         HelpMessage='Enter the End-Port (Default=65535)')]
     [Int32]$EndPort=65535,
 
     [Parameter(
         Position=3,
-        Mandatory=$false,
         HelpMessage='Set the maximum number of threads at the same time (Default=100)')]
     [Int32]$Threads=100,
 
     [Parameter(
         Position=4,
-        Mandatory=$false,
-        HelpMessage='Show closed Ports in result')]
-    [Switch]$IncludeClosed
+        HelpMessage='Update Service Name and Transport Protocol Port Number Registry from IANA.org (https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xml)')]
+    [Switch]$UpdateListFromIANA
 )
 
 Begin{
     # Time when the script starts
-    $StartTime = Get-Date
-
-    # Script FileName
+    $StartTime = Get-Date 
+    
+    # Script Path and FileName
+    $Script_Startup_Path = Split-Path -Parent $MyInvocation.MyCommand.Path
     $ScriptFileName = $MyInvocation.MyCommand.Name
-
+    
+    # IANA -> Service Name and Transport Protocol Port Number Registry -> XML-File
+    $IANA_PortList_WebUri = "https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xml"
+   
+    # Local path to PortList
+    $XML_PortList_Path = "$Script_Startup_Path\ServiceName_and_TransportProtocolPortNumber_Registry.xml"
+            
     # Validate Port-Range
     if($StartPort -gt $EndPort)
     {
@@ -79,6 +86,42 @@ Begin{
     }
     
     $PortRange = ($EndPort - $StartPort)
+        
+    # Port list can be updated from IANA.org with the parameter "-UpdatePortList
+    if($UpdateListFromIANA)
+    {
+        try
+        {
+            Write-Host "Updating Service Name and Transport Protocol Port Number Registry from IANA...`t" -ForegroundColor Gray -NoNewline
+
+            [xml]$New_XML_PortList = Invoke-WebRequest -Uri $IANA_PortList_WebUri # Download latest xml-file from IANA
+
+            Remove-Item $XML_PortList_Path -Force -ErrorAction SilentlyContinue # Don`t show errors if there is no old file
+
+            $New_XML_PortList.Save($XML_PortList_Path) # Save xml-file
+
+            Write-Host "OK" -ForegroundColor Green
+        }
+        catch
+        {
+            $ErrorMsg = $_.Exception.Message
+            
+            Write-Host "Update Service Name and Transport Protocol Port Number Registry from IANA failed with the follwing error message: $ErrorMsg"  -ForegroundColor Red
+        }        
+    }  
+    elseif(-Not([System.IO.File]::Exists($XML_PortList_Path)))
+    {   
+        Write-Host 'No XML-File to assign service name with port number found! Use the parameter "-UpdateListFromIANA" to download the latest version from IANA.org. This warning doesn`t affect the scanning procedure.' -ForegroundColor Yellow
+    }   
+        
+    if([System.IO.File]::Exists($XML_PortList_Path)) 
+    { 
+        $AssignServiceWithPorts = $true 
+    } 
+    else 
+    { 
+        $AssignServiceWithPorts = $false 
+    }
 
     if(-not( Test-Connection -ComputerName $IPv4Address -Count 2 -Quiet))
     {
@@ -107,18 +150,20 @@ Process{
             
             if($Socket.Connected)
             {
-                $Status = "Open"             
+                $Status = "open"             
                 $Socket.Close()
             }
         }
         catch
         {
-            $Status = "Closed"
+            $Status = "closed"
         }
     
         $Result = New-Object -TypeName PSObject
-        Add-Member -InputObject $Result -MemberType NoteProperty -Name Port -Value $Port
-        Add-Member -InputObject $Result -MemberType NoteProperty -Name Status -Value $Status
+        Add-Member -InputObject $Result -MemberType NoteProperty -Name "Port" -Value $Port
+        Add-Member -InputObject $Result -MemberType NoteProperty -Name "Protocol" -Value "tcp"
+        Add-Member -InputObject $Result -MemberType NoteProperty -Name "Status" -Value $Status
+
         return $Result    
     }
     
@@ -164,19 +209,66 @@ Process{
 	Write-Host "Process results...`t`t`t" -ForegroundColor Yellow -NoNewline
 
     # Built global array  
-    $Results = @()
+    $Jobs_Result = @()
 
     # Get results and fill the array
     foreach($Job in $Jobs)
     {
-        $Results += $Job.Pipe.EndInvoke($Job.Result)
+        $Jobs_Result += $Job.Pipe.EndInvoke($Job.Result)
     }
 
+    # Only get open ports (others are closed -.- )
+    $Ports_Open = $Jobs_Result | Where-Object {$_.Status -eq "open"}
+
     Write-Host "[" -ForegroundColor Gray -NoNewline; Write-Host "Done" -ForegroundColor Green -NoNewline; Write-Host "]" -ForegroundColor Gray	
+    
+    # Assign service with ports
+    if($AssignServiceWithPorts)
+    {
+        Write-Host "Assign services to ports...`t`t" -ForegroundColor Yellow -NoNewline
+        
+        $XML_PortList = [xml](Get-Content -Path $XML_PortList_Path)
+
+        $Ports_Open_Assigned = @()
+        
+        # Go through each port
+        foreach($Port_Open in $Ports_Open)
+        {
+            # go through each service
+            foreach($XML_Node in $XML_PortList.Registry.Record)
+            {
+                # Find the right service (based on protocol and port number)
+                if(($Port_Open.Protocol -eq $XML_Node.protocol) -and ($Port_Open.Port -eq $XML_Node.number))
+                {
+                    # Built new custom PSObject
+                    $Port_Open_Assigned = New-Object -TypeName PSObject
+                    Add-Member -InputObject $Port_Open_Assigned -MemberType NoteProperty -Name "Port" -Value $Port_Open.Port
+                    Add-Member -InputObject $Port_Open_Assigned -MemberType NoteProperty -Name "Protocol" -Value $Port_Open.Protocol
+                    Add-Member -InputObject $Port_Open_Assigned -MemberType NoteProperty -Name "Service Name" -Value $XML_Node.name
+                    Add-Member -InputObject $Port_Open_Assigned -MemberType NoteProperty -Name "Service Description" -Value $XML_Node.description
+                    Add-Member -InputObject $Port_Open_Assigned -MemberType NoteProperty -Name "Status" -Value $Port_Open.Status
+
+                    # Add it to an array
+                    $Ports_Open_Assigned += $Port_Open_Assigned
+                }
+            }
+        }        
+        
+        Write-Host "[" -ForegroundColor Gray -NoNewline; Write-Host "Done" -ForegroundColor Green -NoNewline; Write-Host "]" -ForegroundColor Gray	
+    }
 }
 
+End{  
+    # If no XML-File to assign service with port... only show open ports  
+    if($AssignServiceWithPorts) 
+    { 
+        $Results = $Ports_Open_Assigned 
+    } 
+    else 
+    { 
+        $Results = $Ports_Open 
+    }
 
-End{    
     $EndTime = Get-Date
         
     $ExecutionTimeMinutes = (New-TimeSpan -Start $StartTime -End $EndTime).Minutes
@@ -184,12 +276,12 @@ End{
         
     # Some User-Output with Device UP/Down and execution time
     Write-Host "`n+=-=-=-=-=-=-=-=-=-=-=-=  Result  =-=-=-=-=-=-=-=-=-=-=-=`n|"
-    Write-Host "|  Ports Open:`t`t$(@($Results | Where-Object {($_.Status -eq "Open")}).Count)" 
-    Write-Host "|  Ports Closed:`t$(@($Results | Where-Object {($_.Status -eq "Closed")}).Count)"
+    Write-Host "|  Ports Scanned:`t$($Jobs_Result.Count)"
+    Write-Host "|  Ports Open:`t`t$(@($Results | Where-Object {($_.Status -eq "open")}).Count)"     
     Write-Host "|`n+========================================================`n"
     Write-Host "Script duration:`t$ExecutionTimeMinutes Minutes $ExecutionTimeSeconds Seconds`n" -ForegroundColor Yellow
     Write-Host "Script ($ScriptFileName) exit at $EndTime`n" -ForegroundColor Green
             
     # Return custom psobject with Port status
-    if($IncludeClosed) { return $Results } else { return $Results | Where-Object {$_.Status -eq "Open"} } 
+    return $Results
 }
