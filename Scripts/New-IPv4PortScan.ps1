32162f56be3cef6feb07cb74110d620c19632510
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2016-07-13 15:49:17 +0200

    Update New-IPv4PortScan.ps1

--- Scripts/New-IPv4PortScan.ps1
+++ Scripts/New-IPv4PortScan.ps1
@@ -194,7 +194,7 @@ Process{
     {
         Write-Host "$ComputerName is not reachable!" -ForegroundColor Red
 
-        if($Force -eq $false)
+        if($Force.IsPresent -eq $false)
         {
             do {
                 $Answer = Read-Host "Would you like to continue? (perhaps only ICMP is blocked) [yes|no]"
